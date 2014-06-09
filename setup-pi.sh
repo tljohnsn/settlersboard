673de333181c88caa09a3a9e41ba89c658c0fee5
tljohnsn <github@trentjohnson.net>
2014-06-09 13:01:45 -0500

    Update setup-pi.sh

--- setup-pi.sh
+++ setup-pi.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/tljohnsn/settlersboard.git
 
 sudo apt-get -y update
-sudo apt-get -y install libgtk2.0-dev emacs23-nox
+sudo apt-get -y install libgtk2.0-dev emacs23-nox x11-xserver-utils
 mkdir ~/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAwk9g2BbEL7k2DwpEKBNScNspOcZnmGxzBvV/nSdtlXeyuAazr2tIOw13TAl574vneVdFqV+eVwpD4dCVvfatWrBq0byiPVD7kDhu2p6HEK0I9HCn0MsQY56c6Riv4cy/cybp9capOHIx58wrQ6A8DsbZ7wZrsVGYgiUgeZD+tsM= tljohnsn@smack.office.useractive.com
 ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAlRchb+Hm1VM7dYqDZ0D6zCtnf/dH5ba4NkGZ9gFGkHdgQLlepCCYuOUnoCr0N40lQWGdOuCVsWVEsClzRNmLTBXNgF4uDVx+S1Zw/uw/zJ89jWT/WdPr6+hT2s7z8OBe1HGI3zyW7ftzBpUWE9W2qiiPXwUXUAOvn+pX15DPrjE= newlaptopkey
